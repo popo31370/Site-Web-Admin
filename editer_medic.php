94879005b7acf8b3649548225a28e3d6d8a03a78
popo31370 <pauline.rullier@gmail.com>
2023-08-02 22:26:07 +0200

    ajout de la page d'édition d'un medic

--- editer_medic.php
+++ editer_medic.php
@@ -1,3 +1,36 @@
+<?php
+session_start();
+
+$url = "http://localhost:8080/api/medications/";
+
+// Configuration de la requête cURL
+$ch = curl_init();
+
+
+
+$headers = array('X-API-KEY: 3cfa26d6-5c52-480b-90ea-7aee7b40a5d6');
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+
+/*Récupération du champ "id"*/
+if(isset($_GET['id'])) {
+    $url= $url . $_GET['id'];
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+    $response = curl_exec($ch);
+    $response= json_decode($response,true);
+    // Vérification des erreurs
+    if (curl_errno($ch)) {
+        echo 'Erreur cURL : ' . curl_error($ch);
+    }
+}
+
+
+
+// Fermeture de la connexion cURL
+curl_close($ch);
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -66,17 +99,18 @@
         <div class="card">
             <div class="card-body">
                 <h2 class="card-title text-center">Editer un médicament</h2>
-                <form>
+                <form method="POST" action="editer_medic_post.php" enctype="multipart/form-data">
                     <div class="form-group">
                         <label for="nomFr">Nom du médicament en français</label>
-                        <input type="text" class="form-control" id="nomFr" name="nomFr" required>
+                        <input type="text" class="form-control" id="nomFr" name="nomFr" required value="<?php echo $response['nomFr']; ?>">
+                        <input id="id" name="id" type="hidden" value="<?= $_GET['id']; ?>">
                     </div>
                     <div class="form-group">
                         <label for="nomEn">Nom du médicament en anglais</label>
-                        <input type="text" class="form-control" id="nomEn" name="nomEn" required>
+                        <input type="text" class="form-control" id="nomEn" name="nomEn" required value="<?php echo $response['nomEn']; ?>">
                     </div>
                     <div class="text-center">
-                        <button type="submit" class="btn btn-custom">Ajouter</button>
+                        <button type="submit" class="btn btn-custom">Mettre à jour</button>
                     </div>
                 </form>
             </div>
